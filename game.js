7e5a3c99f5d94fc2c2069c75324b51bd33611179
imhiamou <imhiamou@gmail.com>
2026-02-27 17:36:43 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-/* ================= CANVAS ================= */
+* ================= CANVAS ================= */
 
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
